4b317df1372498b26ec413ca7c1fdcbea7e67e46
Chas Emerick <chas@cemerick.com>
2012-03-29 17:23:36 -0400

    lock to nrepl 0.2.0-beta4 for release

--- project.clj
+++ project.clj
@@ -1,17 +1,18 @@
-(defproject com.cemerick/drawbridge "0.0.1-SNAPSHOT"
+(defproject com.cemerick/drawbridge "0.0.1"
   :description "HTTP transport support for Clojure's nREPL implemented as a Ring handler."
   :url "http://github.com/cemerick/drawbridge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  ;:repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/tools.nrepl "0.2.0-SNAPSHOT"]
+                 [org.clojure/tools.nrepl "0.2.0-beta4"]
                  [ring/ring-core "1.0.2"]
                  [cheshire "3.0.0"]
                  
                  ; client
                  [clj-http "0.3.4"]]
-  :dev-dependencies [[ring "1.0.0"]]
+  :dev-dependencies [[ring "1.0.0"]
+                     [lein-clojars "0.7.0"]]
   :profiles {:dev {:dependencies [[ring "1.0.0"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta5"]]}})
