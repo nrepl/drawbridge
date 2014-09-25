9c127c0b99fd38d076df96495fb418c19f6cc4eb
Sam Ritchie <sritchie09@gmail.com>
2014-09-25 10:54:06 -0600

    saving.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.cemerick/drawbridge "0.0.6"
+(defproject racehub/drawbridge "0.1.0"
   :description "HTTP transport support for Clojure's nREPL implemented as a Ring handler."
-  :url "http://github.com/cemerick/drawbridge"
+  :url "http://github.com/racehub/drawbridge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
@@ -10,7 +10,6 @@
 
                  ;; client
                  [clj-http "1.0.0"]]
-  :profiles {:dev {:dependencies [[ring "1.3.0"]]
-                   :plugins [[lein-clojars "0.9.0"]]}
+  :profiles {:dev {:dependencies [[ring "1.3.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :main ^{:skip-aot true} cemerick.drawbridge)
