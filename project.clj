41f7cd47be277b5232d9ba33714ced26799e45c7
Chas Emerick <chas@cemerick.com>
2014-12-15 05:08:45 -0500

    0.0.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cemerick/drawbridge "0.0.6"
+(defproject com.cemerick/drawbridge "0.0.7"
   :description "HTTP transport support for Clojure's nREPL implemented as a Ring handler."
   :url "http://github.com/cemerick/drawbridge"
   :license {:name "Eclipse Public License"
