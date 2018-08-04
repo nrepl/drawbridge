acc22b68529a9ed38bb0f883575a1c33c186b2fa
Bozhidar Batsov <bozhidar@batsov.com>
2018-08-04 08:48:38 +0300

    Bump the nREPL dep

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [nrepl "0.4.0"]
+                 [nrepl "0.4.4"]
                  [ring/ring-core "1.0.2"]
                  [cheshire "3.0.0"]
 
