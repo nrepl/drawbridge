75bb38e936e402e0539a9e01f92d9f7b99e08a3d
Bozhidar Batsov <bozhidar@batsov.com>
2018-11-02 23:49:34 +0200

    Update the deps

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
-(defproject nrepl/drawbridge "0.1.5"
+(defproject nrepl/drawbridge "0.2.0-SNAPSHOT"
   :description "HTTP transport support for Clojure's nREPL implemented as a Ring handler."
   :url "http://github.com/nrepl/drawbridge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [nrepl "0.4.5"]
-                 [ring/ring-core "1.0.2"]
-                 [cheshire "3.0.0"]
+                 [ring/ring-core "1.7.1"]
+                 [cheshire "5.8.1"]
 
                  ;; client
-                 [clj-http "0.3.6"]]
+                 [clj-http "3.9.1"]]
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
                                     :sign-releases false}]]
 
-  :dev-dependencies [[ring "1.0.0"]]
-  :profiles {:dev {:dependencies [[ring "1.0.0"]]}
+  :dev-dependencies [[ring "1.7.1"]]
+  :profiles {:dev {:dependencies [[ring "1.7.1"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
 
