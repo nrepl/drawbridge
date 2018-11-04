453f538412566e3f06bc86e2e0ddb242897ee315
Eero Helenius <eero.helenius@solita.fi>
2018-11-04 22:03:33 +0200

    Refactor assertions to compare response maps #24

--- test/drawbridge/client_test.clj
+++ test/drawbridge/client_test.clj
@@ -19,21 +19,33 @@
 
 (use-fixtures :once server-fixture)
 
+(defn without-unstable-keys
+  [res]
+  (map #(dissoc % :id :session) res))
+
 (defn send-message
   [client message]
   (-> client
       (nrepl/message message)
-      nrepl/response-values
-      last))
+      without-unstable-keys
+      first))
 
 (deftest sending-messages
   (with-open [conn (nrepl/url-connect "http://localhost:12345/repl")]
     (let [client (nrepl/client conn 1000)]
-      (testing "Evaluating valid form returns the result"
-        (is (= 3 (send-message client {:op "eval" :code "(+ 1 2)"}))))
+      (testing "Evaluating a valid form returns the value"
+        (is (= {:value "3"
+                :ns    "user"}
+               (send-message client {:op "eval" :code "(+ 1 2)"}))))
 
-      (testing "Evaluating invalid form returns nil"
-        (is (nil? (send-message client {:op "eval" :code "(+ 1 2"}))))
+      (testing "Evaluating an invalid form returns an error"
+        (is (= {:status  ["eval-error"]
+                :ex      "class clojure.lang.LispReader$ReaderException"
+                :root-ex "class java.lang.RuntimeException"}
+               (send-message client {:op "eval" :code "(+ 1 2"}))))
 
-      (testing "Evaluating a form that throws returns nil"
-        (is (nil? (send-message client {:op "eval" :code "(throw (ex-info nil {:foo :bar}))"})))))))
+      (testing "Evaluating a form that throws returns an error"
+        (is (= {:status  ["eval-error"]
+                :ex      "class clojure.lang.ExceptionInfo"
+                :root-ex "class clojure.lang.ExceptionInfo"}
+               (send-message client {:op "eval" :code "(throw (ex-info nil {:foo :bar}))"})))))))
