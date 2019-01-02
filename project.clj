3c8b5c7381e31514538f4c1b651038ed8e7ccaec
Bozhidar Batsov <bozhidar@batsov.com>
2019-01-02 17:47:15 +0200

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nrepl/drawbridge "0.2.0-SNAPSHOT"
+(defproject nrepl/drawbridge "0.2.0"
   :description "HTTP transport support for Clojure's nREPL implemented as a Ring handler."
   :url "http://github.com/nrepl/drawbridge"
   :license {:name "Eclipse Public License"
