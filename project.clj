cdcd19301f388f05adc16d0e85e7f34d47e4a380
Bozhidar Batsov <bozhidar@batsov.dev>
2026-07-09 18:55:44 +0300

    Bump nREPL, Ring, and Cheshire to latest

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/nrepl/drawbridge"}
-  :dependencies [[nrepl "1.5.2"]
-                 [ring/ring-core "1.15.3"]
-                 [cheshire "6.1.0"]
+  :dependencies [[nrepl "1.7.0"]
+                 [ring/ring-core "1.15.5"]
+                 [cheshire "6.2.0"]
                  ;; client
                  [clj-http "3.13.1"]]
 
@@ -18,8 +18,8 @@
                                     :sign-releases false}]]
 
   :profiles {:dev {:dependencies [[compojure "1.7.2"]
-                                  [ring/ring-jetty-adapter "1.15.3"]
-                                  [ring "1.15.3"]]}
+                                  [ring/ring-jetty-adapter "1.15.5"]
+                                  [ring "1.15.5"]]}
              :provided {:dependencies [[org.clojure/clojure "1.10.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
              :1.11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
